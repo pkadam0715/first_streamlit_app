b3403a2f095176eb23df9b831de78918a0821e22
pkadam0715 <133630284+pkadam0715@users.noreply.github.com>
2023-05-15 12:30:04 +0530

    Update streamlit_app.py
    Another Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ streamlit.text('🥗 poha, Upma and Idli Samber')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥑🍞 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
