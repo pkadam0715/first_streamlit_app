66797ad8e3cf2643744452fcbecb6a7388eb9435
pkadam0715 <133630284+pkadam0715@users.noreply.github.com>
2023-05-17 21:44:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,3 +63,8 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+# take the json version of response and normalize it 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output it the screen as a table 
+streamlit.dataframe(fruityvice_normalized)
