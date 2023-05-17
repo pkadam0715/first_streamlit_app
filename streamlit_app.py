54483f16bd5b0b7604792c99857978c1bc1c5c4c
pkadam0715 <133630284+pkadam0715@users.noreply.github.com>
2023-05-17 21:20:12 +0530

    Update streamlit_app.py
    Fruityvice API

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,3 +30,8 @@ streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index), ['Avocado'
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(my_fruit_list)
+
+# New section to display fruityvice API Response
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
