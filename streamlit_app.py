7f43119de589baef906a4767630793c3e5c47ffa
pkadam0715 <133630284+pkadam0715@users.noreply.github.com>
2023-05-18 14:08:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 import streamlit 
 
 streamlit.title('God is Great')
@@ -11,7 +17,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
@@ -45,7 +51,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table 
 streamlit.dataframe(fruityvice_normalized)
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # take the json version of response and normalize it 
@@ -64,7 +70,7 @@ import snowflake.connector
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # take the json version of response and normalize it 
@@ -101,8 +107,17 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('what food would you like to add')
 streamlit.write('Thanks for adding', add_my_fruit)
 
-#This will not work correctly, but just go with it for now
-my_CUI.execute("insert into fruit_load_list_values ('from streamlit')")
+#don't run anything past here while we troubleshoot
+streamlit.stop()
+
+import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.text("The fruit load list contains:")
+streamlit.text(my_data_rows)
 
 
 
