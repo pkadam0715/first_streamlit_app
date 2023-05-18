9e76366378dc4a9198d81e5c4582e9729cd108f2
pkadam0715 <133630284+pkadam0715@users.noreply.github.com>
2023-05-18 13:13:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,3 +80,5 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
+select * from pc_rivery_db.public.fruit_load_list
