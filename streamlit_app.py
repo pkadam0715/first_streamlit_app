3d4ad2ee3f61240be62d649bddc44b5c6f80b17e
pkadam0715 <133630284+pkadam0715@users.noreply.github.com>
2023-05-19 20:17:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -133,39 +133,20 @@ except URLError as e:
     streamlit.error()
     
 #create the repeatable code block (called a function)
-#def get_fruityvice_data(this_fruit_choice):
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#return fruityvice_normalized
+def get_fruityvice_data(this_fruit_choice):
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+return fruityvice_normalized
 
-# New section to display fruityvice API Response
-#streamlit.header('Fruityvice Fruit Advice!')
-#try:
-    #fruit_choice = streamlit.text_input('what fruit would you like information about?')
-    #if not fruit_choice:
-       #streamlit.error("please select a fruit to get information.")
-    #else:
-      # back_from_function = get_fruityvice_data(fruit_choice)
-       #streamlit.dataframe(back_from_function)
+#New section to display fruityvice API Response
+streamlit.header('Fruityvice Fruit Advice!')
+try:
+    fruit_choice = streamlit.text_input('what fruit would you like information about?')
+    if not fruit_choice:
+       streamlit.error("please select a fruit to get information.")
+    else:
+      back_from_function = get_fruityvice_data(fruit_choice)
+       streamlit.dataframe(back_from_function)
         
-streamlit.header("The fruit load list contains:")
-#snowflake-related functions
-def get_fruit_load_list():
-with my_cnx.cursor() as my_cur:
-my_cur.execute("select * from fruit_load_list")
-return my_cur.fetchall()
-
-#Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_data_rows = get_fruit_load_list()
-streamlit.dataframe(my_data_rows)
-
-
-        
-        
-
-
-
 
 
