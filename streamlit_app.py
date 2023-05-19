820bc51f4107f0ea9aa1044ffaf9001df10a97ec
pkadam0715 <133630284+pkadam0715@users.noreply.github.com>
2023-05-19 20:22:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -120,17 +120,17 @@ streamlit.text(my_data_rows)
 
 streamlit.header('Fruityvice Fruit Advice!')
 try:
-    fruit_choice = streamlit.text_input('what fruit would you like information about?')
-    streamlit.write('The user entered ', fruit_choice)
+fruit_choice = streamlit.text_input('what fruit would you like information about?')
+streamlit.write('The user entered ', fruit_choice)
 
-    if not fruit_choice:
-       streamlit.error("please select a fruit to get information.")
-    else:
-       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-       streamlit.dataframe(fruityvice_normalized)
+if not fruit_choice:
+streamlit.error("please select a fruit to get information.")
+else:
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
-    streamlit.error()
+streamlit.error()
     
 #create the repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
@@ -141,12 +141,12 @@ return fruityvice_normalized
 #New section to display fruityvice API Response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
-    fruit_choice = streamlit.text_input('what fruit would you like information about?')
-    if not fruit_choice:
-       streamlit.error("please select a fruit to get information.")
-    else:
-      back_from_function = get_fruityvice_data(fruit_choice)
-       streamlit.dataframe(back_from_function)
+fruit_choice = streamlit.text_input('what fruit would you like information about?')
+if not fruit_choice:
+streamlit.error("please select a fruit to get information.")
+else:
+back_from_function = get_fruityvice_data(fruit_choice)
+streamlit.dataframe(back_from_function)
         
 
 
