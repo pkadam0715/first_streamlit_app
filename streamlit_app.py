0e6e35d1e24dbd748599cafe09890daf6d1a018a
pkadam0715 <133630284+pkadam0715@users.noreply.github.com>
2023-05-19 21:05:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -138,7 +138,6 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
-#New section to display fruityvice API Response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
     fruit_choice = streamlit.text_input('what fruit would you like information about?')
