e9a56dfc93632d2a0b5785979c9a2a22d6fadc7e
pkadam0715 <133630284+pkadam0715@users.noreply.github.com>
2023-05-19 13:27:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -133,7 +133,7 @@ except URLError as e:
     streamlit.error()
     
 #create the repeatable code block (called a function)
-def get_fruityvice_data(this_fruit_choice):
+def get_fruityvice_data(this_fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 return fruityvice_normalized
