e9aeb37e294f64d21a1172b4bb8e3cfd296eaa78
pkadam0715 <133630284+pkadam0715@users.noreply.github.com>
2023-05-22 15:45:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -131,3 +131,7 @@ try:
 except URLError as e:
     streamlit.error()
     
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
