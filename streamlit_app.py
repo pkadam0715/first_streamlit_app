8a33f34e2cc1e41f32231d5512a43a44ba27b5ee
pkadam0715 <133630284+pkadam0715@users.noreply.github.com>
2023-05-22 15:31:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,12 +123,12 @@ try:
     fruit_choice = streamlit.text_input('what fruit would you like information about?')
     streamlit.write('The user entered ', fruit_choice)
 
-    if not fruit_choice:
-        streamlit.error("please select a fruit to get information.")
-    else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+if not fruit_choice:
+    streamlit.error("please select a fruit to get information.")
+else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
     streamlit.error()
     
@@ -142,8 +142,8 @@ def get_fruityvice_data(this_fruit_choice):
 streamlit.header('Fruityvice Fruit Advice!')
 try:
     fruit_choice = streamlit.text_input ('what fruit would you like information about?')
-    if not fruit_choice:
-        streamlit.error("please select a fruit to get information.")
-    else:
-        back_from_function = get_fruityvice_data(fruit_choice)
-        streamlit.dataframe(back_from_function)
+if not fruit_choice:
+    streamlit.error("please select a fruit to get information.")
+else:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
