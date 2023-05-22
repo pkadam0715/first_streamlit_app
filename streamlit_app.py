beb864f41730ec38d0bb12e03aeb0299bf1cb06b
pkadam0715 <133630284+pkadam0715@users.noreply.github.com>
2023-05-22 15:03:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -129,8 +129,8 @@ try:
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
         streamlit.dataframe(fruityvice_normalized)
-    except URLError as e:
-        streamlit.error()
+except URLError as e:
+    streamlit.error()
     
 #create the repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
@@ -142,6 +142,7 @@ def get_fruityvice_data(this_fruit_choice):
 streamlit.header('Fruityvice Fruit Advice!')
 try:
     fruit_choice = streamlit.text_input ('what fruit would you like information about?')
+    
     if not fruit_choice:
         streamlit.error("please select a fruit to get information.")
     else:
