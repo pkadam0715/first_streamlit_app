d038864a9f78340ffa2179230eec744c30ad9fbe
pkadam0715 <133630284+pkadam0715@users.noreply.github.com>
2023-05-22 15:37:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,7 +122,6 @@ streamlit.header('Fruityvice Fruit Advice!')
 try:
     fruit_choice = streamlit.text_input('what fruit would you like information about?')
     streamlit.write('The user entered ', fruit_choice)
-
 if not fruit_choice:
     streamlit.error("please select a fruit to get information.")
 else:
