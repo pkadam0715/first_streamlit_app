49c15db224bce76db42b4dcfe0674600343b7958
pkadam0715 <133630284+pkadam0715@users.noreply.github.com>
2023-05-23 11:08:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -131,12 +131,12 @@ try:
 except URLError as e:
 	streamlit.error()
     
-'''def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
+def get_fruityvice_data(this_fruit_choice):
+	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+	fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+	return fruityvice_normalized
 streamlit.header('Fruityvice Fruit Advice!')
-try:
+'''try:
     fruit_choice = streamlit.text_input('what fruit would you like information about?')
     if not fruit_choice:
         streamlit.error("please select a fruit to get information.")
